3cfe8043b7262bee136256af5a16ae1ddaefcde6
Ridho Ray Putra <ridhorayputra033@gmail.com>
2023-08-26 13:44:56 +0700

    Tipe data string

--- src/TipeDataBoolean.java
+++ src/TipeDataBoolean.java
@@ -1,5 +1,5 @@
 public class TipeDataBoolean {
-t a    public static void main(String[] args) {
+    public static void main(String[] args) {
         boolean Benar = true;
         boolean Salah = false;
 
---
+++ src/TipeDataString.java
@@ -0,0 +1,14 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String FirstName = "Ridho";
+        String LastName = "Ray";
+
+        System.out.println(FirstName);
+        System.out.println(LastName);
+
+        String FullName = FirstName + " " + LastName;
+
+        System.out.println(FullName);
+
+    }
+}
