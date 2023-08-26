355a6f1f7ea885bc4e0472cf3c6a7a0b9f3a4396
Ridho Ray Putra <ridhorayputra033@gmail.com>
2023-08-26 12:32:40 +0700

    tipe data char

---
+++ src/TipeDataChar.java
@@ -0,0 +1,14 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char r = 'R';
+        char a = 'A';
+        char y = 'Y';
+
+        System.out.println(r);
+//        Ketika tidak ingin menambhakn line baru bisa pakai
+        System.out.print(r);
+        System.out.print(a);
+        System.out.print(y);
+
+    }
+}
