197b7aa81a72ea5fccfc190c61b61fafc29ba999
Ridho Ray Putra <ridhorayputra033@gmail.com>
2023-08-26 13:27:06 +0700

    Tipe Data Boolean

---
+++ src/TipeDataBoolean.java
@@ -0,0 +1,9 @@
+public class TipeDataBoolean {
+t a    public static void main(String[] args) {
+        boolean Benar = true;
+        boolean Salah = false;
+
+        System.out.println(Benar);
+        System.out.println(Salah);
+    }
+}
